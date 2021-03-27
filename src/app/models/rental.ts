4121f3db1df17f4d00b9564f408da0b6b9c3afab
ALİERK KÜÇÜK <alierkkucuk@hotmail.com>
2021-03-27 14:31:06 +0300

    27.03.2021

---
+++ src/app/models/rental.ts
@@ -0,0 +1,9 @@
+export interface Rental{
+    rentalId:number;
+    carId:number;
+    brandName:string;
+    colorName:string;
+    customerName:string;
+    rentDate:Date;
+    returnDate:Date;
+}
